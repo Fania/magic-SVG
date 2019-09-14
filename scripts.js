b605cb665ba7cdc9a95077f891fc2d3802fa4ccc
Fania Ra <fania@web.de>
2019-09-14 14:19:10 +0100

    full curved line

--- scripts.js
+++ scripts.js
@@ -85,105 +85,50 @@
 
 
     // rounded corners
-    // console.log(arr);
-    // coords += `M${arr[1][1] * sizeInc},${arr[1][0] * sizeInc} `;
-
-    for (let a=1; a <= (Object.keys(arr).length - 2); a++) {
-      console.log("a: ", a);
+    for (let a=1; a <= Object.keys(arr).length; a++) {
       // coords += `${arr[a][1] * sizeInc},${arr[a][0] * sizeInc} `;
       // coords += `A 30,30 0,0 1 ${arr[a][1] * sizeInc},${arr[a][0] * sizeInc} `;
       // coords += `Q ${arr[a][1] * sizeInc},${arr[a][0] * sizeInc} ${arr[a+1][1] * sizeInc},${arr[a+1][0] * sizeInc} `;
 
       let c1x = arr[a][1] * sizeInc;
       let c1y = arr[a][0] * sizeInc;
+      let c2x, c2y, c3x, c3y;
 
-      let c2x = arr[a+1][1] * sizeInc;
-      let c2y = arr[a+1][0] * sizeInc;
+      // next to last
+      if (a == (Object.keys(arr).length)) {
+        c2x = arr[1][1] * sizeInc;
+        c2y = arr[1][0] * sizeInc;
+        c3x = arr[2][1] * sizeInc;
+        c3y = arr[2][0] * sizeInc;
 
-      let c3x = arr[a+2][1] * sizeInc;
-      let c3y = arr[a+2][0] * sizeInc;
+      // last
+      } else if (a == (Object.keys(arr).length - 1)) {
+        c2x = arr[a+1][1] * sizeInc;
+        c2y = arr[a+1][0] * sizeInc;
+        c3x = arr[1][1] * sizeInc;
+        c3y = arr[1][0] * sizeInc;
+
+      // all previous
+      } else {
+        c2x = arr[a+1][1] * sizeInc;
+        c2y = arr[a+1][0] * sizeInc;
+        c3x = arr[a+2][1] * sizeInc;
+        c3y = arr[a+2][0] * sizeInc;
+      }
 
       let m1x = (c1x + c2x) / 2;
       let m1y = (c1y + c2y) / 2;
-
       let m2x = (c2x + c3x) / 2;
       let m2y = (c2y + c3y) / 2;
 
-      console.log(c1x, c1y, m1x, m1y, c2x, c2y, m2x, m2y, c3x, c3y);
-
+      // console.log(c1x, c1y, m1x, m1y, c2x, c2y, m2x, m2y, c3x, c3y);
       coords += `M ${m1x},${m1y} Q ${c2x},${c2y} ${m2x},${m2y} `;
+
     }
-    // coords += `M ${arr[1][1] * sizeInc},${arr[1][0] * sizeInc} `;  // loop back
 
     let svgPath = `<svg class="order-x" style="fill: ${svgFill}; stroke: ${svgStroke}" viewbox="${-pad} ${-pad} ${w-sizeInc+pad+pad} ${w-sizeInc+pad+pad}"><path id="lines" d="${coords}"/></svg>`;
     console.log(svgPath);
     svgGrid.innerHTML += svgPath;
 
-// first order 6
-
-/* 
-M 300,200 Q 100,300 100,200 
-M 150,200 Q 200,300 300,400 
-M 450,400 Q 500,300 250,400 
-M 150,300 Q 300,100 250,250 
-M 300,200 Q 400,0 200,100 
-M 0,100 Q 0,0 100,100 
-M 300,300 Q 400,400 400,300 
-M 200,300 Q 0,400 100,200 
-M 250,200 Q 300,400 400,200 
-M 300,100 Q 100,200 100,100 
-M 200,100 Q 300,200 400,300 
-M 500,300 Q 500,200 300,300 
-M 200,200 Q 300,0 250,250 
-M 300,300 Q 400,100 200,200 
-M 0,200 Q 0,100 150,200 
-M 400,400 Q 500,500 450,400 
-M 250,400 Q 100,500 150,300 */
-
-/* 
-300,500 
-500,100 
-100,300 
-100,100 
-200,300 
-400,500 
-500,300 
-0,500 
-300,100 
-200,400 
-400,0 
-0,200 
-0,0 
-200,200 
-400,400 
-400,200 
-0,400 
-200,0 
-300,400 
-500,0 
-100,200 
-100,0 
-300,200 
-500,400 
-500,200 
-100,400 
-300,0 
-200,500 
-400,100 
-0,300 
-0,100 
-300,300 
-500,500 
-400,300
-100,500 
-200,100 
-300,500 */
-
-
-
-    // <svg class="order-x" style="fill: transparent; stroke: white" viewbox="-30 -30 560 560"><path id="lines" d="M100,200 Q 100,300 400,400 Q 400,500 200,0 Q 300,100 500,100 Q 400,0 300,200 Q 300,300 0,500 Q 100,400 200,400 Q 200,500 0,100 Q 0,0 500,300 Q 500,200 0,300 Q 0,200 500,500 Q 500,400 300,0 Q 200,100 400,100 Q 500,0 200,200 Q 200,300 100,500 Q 0,400 300,400 Q 300,500 100,0 Q 100,100 400,300 M 100,200 "/></svg>
-
-    // <path d="M100,100 h200 a20,20 0 0 1 20,20 v200 a20,20 0 0 1 -20,20 h-200 a20,20 0 0 1 -20,-20 v-200 a20,20 0 0 1 20,-20 z" />
-    // a20,20 0 0 1 20,20  svg arc rounded corner
   }
 })();
